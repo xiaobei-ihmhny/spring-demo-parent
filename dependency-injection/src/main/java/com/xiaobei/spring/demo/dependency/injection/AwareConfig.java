bd974560d1a1452804705f18d1fa1a0d03891a9a
zhibeibei <Z471200a>
2020-08-05 13:47:50 +0800

    59 接口回调注入：回调注入的使用场景和限制有哪些？

---
+++ dependency-injection/src/main/java/com/xiaobei/spring/demo/dependency/injection/AwareConfig.java
@@ -0,0 +1,134 @@
+package com.xiaobei.spring.demo.dependency.injection;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.*;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.StringValueResolver;
+
+/**
+ * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
+ * @date 2020-08-05 13:27:27
+ */
+public class AwareConfig implements BeanFactoryAware, ApplicationContextAware,
+        EnvironmentAware, ResourceLoaderAware, BeanClassLoaderAware,
+        BeanNameAware, MessageSourceAware, ApplicationEventPublisherAware,
+        EmbeddedValueResolverAware {
+    private ClassLoader classLoader;
+
+    private BeanFactory beanFactory;
+
+    private String name;
+
+    private ApplicationContext applicationContext;
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    private StringValueResolver resolver;
+
+    private Environment environment;
+
+    private MessageSource messageSource;
+
+    private ResourceLoader resourceLoader;
+
+    public ClassLoader getClassLoader() {
+        return classLoader;
+    }
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public ApplicationEventPublisher getApplicationEventPublisher() {
+        return applicationEventPublisher;
+    }
+
+    public StringValueResolver getResolver() {
+        return resolver;
+    }
+
+    public Environment getEnvironment() {
+        return environment;
+    }
+
+    public MessageSource getMessageSource() {
+        return messageSource;
+    }
+
+    public ResourceLoader getResourceLoader() {
+        return resourceLoader;
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @Override
+    public void setEmbeddedValueResolver(StringValueResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
+    @Override
+    public String toString() {
+        return "AwareConfig{" +
+                "classLoader=" + classLoader +
+                ", beanFactory=" + beanFactory +
+                ", name='" + name + '\'' +
+                ", applicationContext=" + applicationContext +
+                ", applicationEventPublisher=" + applicationEventPublisher +
+                ", resolver=" + resolver +
+                ", environment=" + environment +
+                ", messageSource=" + messageSource +
+                ", resourceLoader=" + resourceLoader +
+                '}';
+    }
+}
---
+++ dependency-injection/src/main/java/com/xiaobei/spring/demo/dependency/injection/AwareInterfaceDependencyInjectionDemo.java
@@ -0,0 +1,31 @@
+package com.xiaobei.spring.demo.dependency.injection;
+
+import org.junit.Test;
+import org.springframework.beans.factory.Aware;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
+ * @date 2020-08-05 13:27:27
+ */
+public class AwareInterfaceDependencyInjectionDemo {
+
+    /**
+     * 通过{@link Aware} 接口来“感知”到相关的对象，
+     * 这和spring提供的内建的依赖是同一个对象，只是实现手段不同
+     */
+    @Test
+    public void awareInterfaceDependencyInjection() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(AwareConfig.class);
+        applicationContext.refresh();
+        AwareConfig awareConfig = applicationContext.getBean(AwareConfig.class);
+        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+        System.out.println(awareConfig.getBeanFactory() == beanFactory);
+        System.out.println(awareConfig.getApplicationContext() == applicationContext);
+        System.out.println(awareConfig.toString());
+        applicationContext.close();
+
+    }
+}
