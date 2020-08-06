d47bcd454f8162199e8257604ce52272f9f3559c
xiaobei <legend0508@163.com>
2020-08-06 22:29:10 +0800

    72 依赖注入的来源：难道依赖注入的来源与依赖查找的不同吗？

---
+++ dependency-source/src/main/java/com/xiaobei/spring/demo/dependency/source/DependencySourceDemo.java
@@ -0,0 +1,80 @@
+package com.xiaobei.spring.demo.dependency.source;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.ResourceLoader;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 依赖来源示例
+ * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
+ * @date 2020/8/6 22:16
+ */
+public class DependencySourceDemo {
+
+    @Autowired
+    private BeanFactory beanFactory;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    /**
+     * application == beanFactory：false
+     * beanFactory == application.getBeanFactory：true
+     * application == applicationEventPublisher：true
+     * application == resourceLoader：true
+     */
+    @PostConstruct
+    public void initByInject() {
+        System.out.println("application == beanFactory：" + (applicationContext == beanFactory));
+        System.out.println("beanFactory == application.getBeanFactory："
+                + (beanFactory == applicationContext.getAutowireCapableBeanFactory()));
+        System.out.println("application == applicationEventPublisher：" + (applicationContext == applicationEventPublisher));
+        System.out.println("application == resourceLoader：" + (applicationContext == resourceLoader));
+    }
+
+    /**
+     * No qualifying bean of type 'org.springframework.beans.factory.BeanFactory' available
+     * No qualifying bean of type 'org.springframework.context.ApplicationContext' available
+     * No qualifying bean of type 'org.springframework.context.ApplicationEventPublisher' available
+     * No qualifying bean of type 'org.springframework.core.io.ResourceLoader' available
+     */
+    @PostConstruct
+    public void initByLookup() {
+        getBean(BeanFactory.class);
+        getBean(ApplicationContext.class);
+        getBean(ApplicationEventPublisher.class);
+        getBean(ResourceLoader.class);
+    }
+
+    public <T> T getBean(Class<T> classType) {
+        try {
+            return beanFactory.getBean(classType);
+        } catch (NoSuchBeanDefinitionException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(DependencySourceDemo.class);
+        applicationContext.refresh();
+        applicationContext.close();
+    }
+}
