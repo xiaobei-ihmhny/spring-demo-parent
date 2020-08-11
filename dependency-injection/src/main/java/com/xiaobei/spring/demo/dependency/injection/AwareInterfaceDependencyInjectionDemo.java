55799c54f52c82f2681c292dbc00808f035ef67c
xiaobei <legend0508@163.com>
2020-08-11 23:15:11 +0800

    97 Aware接口回调阶段：众多Aware接口回调的顺序是安排的？

--- dependency-injection/src/main/java/com/xiaobei/spring/demo/dependency/injection/AwareInterfaceDependencyInjectionDemo.java
+++ dependency-injection/src/main/java/com/xiaobei/spring/demo/dependency/injection/AwareInterfaceDependencyInjectionDemo.java
@@ -4,7 +4,17 @@ import com.xiaobei.spring.demo.dependency.config.AwareConfig;
 import org.junit.Test;
 import org.springframework.beans.factory.Aware;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.*;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.StringValueResolver;
 
 /**
  * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
@@ -15,6 +25,20 @@ public class AwareInterfaceDependencyInjectionDemo {
     /**
      * 通过{@link Aware} 接口来“感知”到相关的对象，
      * 这和spring提供的内建的依赖是同一个对象，只是实现手段不同
+     *
+     * 其中 {@link BeanNameAware} {@link BeanClassLoaderAware} {@link BeanFactoryAware}
+     * 的回调位置位于：
+     * {@link AbstractAutowireCapableBeanFactory#invokeAwareMethods(java.lang.String, java.lang.Object)}
+     *
+     * 其中 {@link EnvironmentAware} {@link EmbeddedValueResolverAware} {@link ResourceLoaderAware}
+     * {@link ApplicationEventPublisherAware} {@link MessageSourceAware} {@link ApplicationContextAware}
+     * 的回调位置位于：
+     * {@link org.springframework.context.support.ApplicationContextAwareProcessor#invokeAwareInterfaces(java.lang.Object)}
+     *
+     * 参见 {@link AbstractApplicationContext#prepareBeanFactory(ConfigurableListableBeanFactory)}
+     * 中的方法 {@code beanFactory.addBeanPostProcessor(new ApplicationContextAwareProcessor(this));}
+     *
+     * @see AbstractApplicationContext#prepareBeanFactory(ConfigurableListableBeanFactory)
      */
     @Test
     public void awareInterfaceDependencyInjection() {
