1589899537bfad65241134d8ff653745444ba09b
zhibeibei <Z471200a>
2020-08-12 10:00:49 +0800

    99 Spring Bean初始化阶段：@PostConstruct、InitializingBean以及自定义方法

--- bean-lifecycle/src/main/java/com/xiaobei/spring/demo/bean/lifecycle/BeanInitializationLifecycleDemo.java
+++ bean-lifecycle/src/main/java/com/xiaobei/spring/demo/bean/lifecycle/BeanInitializationLifecycleDemo.java
@@ -3,6 +3,7 @@ package com.xiaobei.spring.demo.bean.lifecycle;
 import com.xiaobei.spring.demo.bean.lifecycle.domain.LifeCycleDomain;
 import org.junit.Test;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.InitDestroyAnnotationBeanPostProcessor;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory;
@@ -10,7 +11,10 @@ import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
 import org.springframework.util.ObjectUtils;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
@@ -65,5 +69,41 @@ public class BeanInitializationLifecycleDemo {
         System.out.println(lifeCycleDomain);
         LifeCycleDomain superLifeCycle = beanFactory.getBean("superLifeCycle", LifeCycleDomain.class);
         System.out.println(superLifeCycle);
+    }
+
+    /**
+     * 99 Spring Bean初始化阶段：@PostConstruct、InitializingBean以及自定义方法
+     * 1. 需要在 {@code META-INF/life-cycle-domain.xml} 文件中添加 init-method="init"，来激活init自定义初始化方法
+     * 2. 需要在当前的 {@code beanFactory}中添加 {@link CommonAnnotationBeanPostProcessor} 使其具有解析 {@link PostConstruct}的能力
+     *
+     * 运行结果：
+     * postConstruct() -> LifeCycleDomain{id=2, name='xiaobei-ihmhny', city=BIEJING}
+     * afterPropertiesSet() -> LifeCycleDomain{id=2, name='xiaobei', city=BIEJING}
+     * postConstruct() -> LifeCycleDomain{id=2, name='xiaobei_ihmhny', city=BIEJING}
+     * 最终获得的 lifeCycleDomain为：LifeCycleDomain{id=2, name='xiaobei_ihmhny', city=BIEJING}
+     *
+     * @see AbstractAutowireCapableBeanFactory#initializeBean(java.lang.String, java.lang.Object, RootBeanDefinition)
+     * 其中在 {@code wrappedBean = applyBeanPostProcessorsBeforeInitialization(wrappedBean, beanName);}中完成对 {@link PostConstruct}的解析
+     * @see AbstractAutowireCapableBeanFactory#applyBeanPostProcessorsBeforeInitialization(java.lang.Object, java.lang.String)
+     * 其中的 {@code Object current = processor.postProcessBeforeInitialization(result, beanName);}
+     * @see InitDestroyAnnotationBeanPostProcessor#buildLifecycleMetadata(java.lang.Class)
+     * 其中的{@code if (this.initAnnotationType != null && method.isAnnotationPresent(this.initAnnotationType))}
+     * @see InitDestroyAnnotationBeanPostProcessor.LifecycleMetadata#invokeInitMethods(java.lang.Object, java.lang.String)
+     * @see InitDestroyAnnotationBeanPostProcessor.LifecycleElement#invoke(java.lang.Object)
+     *
+     * @see PostConstruct
+     * @see CommonAnnotationBeanPostProcessor
+     */
+    @Test
+    public void initialization() {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        String location = "META-INF/life-cycle-domain.xml";
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions(location);
+        // 添加注解驱动支持
+        beanFactory.addBeanPostProcessor(new CommonAnnotationBeanPostProcessor());
+        // 依赖查找
+        LifeCycleDomain lifeCycleDomain = beanFactory.getBean("lifeCycleDomain", LifeCycleDomain.class);
+        System.out.println("最终获得的 lifeCycleDomain为：" + lifeCycleDomain);
     }
 }
--- bean-lifecycle/src/main/java/com/xiaobei/spring/demo/bean/lifecycle/domain/LifeCycleDomain.java
+++ bean-lifecycle/src/main/java/com/xiaobei/spring/demo/bean/lifecycle/domain/LifeCycleDomain.java
@@ -1,10 +1,14 @@
 package com.xiaobei.spring.demo.bean.lifecycle.domain;
 
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+
 /**
  * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
  * @date 2020/8/9 15:17
  */
-public class LifeCycleDomain {
+public class LifeCycleDomain implements InitializingBean {
 
     private Long id;
 
@@ -46,5 +50,35 @@ public class LifeCycleDomain {
                 ", name='" + name + '\'' +
                 ", city=" + city +
                 '}';
+    }
+
+    /**
+     * 该方法最先执行，前提是当前的BeanFactory得有注解驱动能力！！
+     * @see org.springframework.beans.factory.BeanFactory
+     * @see org.springframework.context.annotation.CommonAnnotationBeanPostProcessor
+     */
+    @PostConstruct
+    public void postConstruct() {
+        this.name = "xiaobei-ihmhny";
+        System.out.println("postConstruct() -> " + this);
+    }
+
+    /**
+     * 该方法在 {@link PostConstruct}之后执行
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.name = "xiaobei";
+        System.out.println("afterPropertiesSet() -> " + this);
+    }
+
+    /**
+     * 该方法在{@link InitializingBean#afterPropertiesSet()}之后。
+     * 该方法最后执行
+     */
+    public void init() {
+        this.name = "xiaobei_ihmhny";
+        System.out.println("postConstruct() -> " + this);
     }
 }
