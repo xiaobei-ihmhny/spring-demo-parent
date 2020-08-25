9b20354c4d117b10fde4371dde5902ce13736657
xiaobei <legend0508@163.com>
2020-08-25 14:34:15 +0800

    198 Spring 4.1事件异常处理：ErrorHandler使用有怎样的限制？

--- event/src/main/java/com/xiaobei/spring/demo/event/AsyncEventHandlerDemo.java
+++ event/src/main/java/com/xiaobei/spring/demo/event/AsyncEventHandlerDemo.java
@@ -1,6 +1,8 @@
 package com.xiaobei.spring.demo.event;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -27,11 +30,24 @@ import static org.springframework.context.support.AbstractApplicationContext.APP
 public class AsyncEventHandlerDemo {
 
     public static void main(String[] args) {
+        // [线程 ：main] 监听到事件：com.xiaobei.spring.demo.event.MyApplicationEvent[source=Hello, World]
 //        syncApplicationEvent();
+        // [线程 ：pool-1-thread-1] 监听到事件：com.xiaobei.spring.demo.event.MyApplicationEvent[source=Hello, World]
+        // 当前操作发生异常，异常信息为：java.lang.RuntimeException: 发生异常
 //        asyncApplicationEventDefaultThreadFactory();
+        // [线程 ：my-event-thread-pool1] 监听到事件：com.xiaobei.spring.demo.event.MyApplicationEvent[source=Hello, World]
+        // 当前操作发生异常，异常信息为：java.lang.RuntimeException: 发生异常
 //        asyncApplicationEventCustomizedThreadFactory();
+        // @Autowired方式配置 Multicaster时，事件发生异常 java.lang.RuntimeException: 发生异常....
+        // [线程 ：main] 监听到事件：com.xiaobei.spring.demo.event.MyApplicationEvent[source=Hello, World]
 //        syncApplicationEventByAnnotation();
+        // @Autowired方式配置 Multicaster时，事件发生异常 java.lang.RuntimeException: 发生异常....
+        // 八月 25, 2020 2:33:26 下午 org.springframework.aop.interceptor.AsyncExecutionAspectSupport getDefaultExecutor
+        // 信息: No task executor bean found for async processing: no bean of type TaskExecutor and no bean named 'taskExecutor' either
+        // [线程 ：SimpleAsyncTaskExecutor-1] 监听到事件：com.xiaobei.spring.demo.event.MyApplicationEvent[source=Hello, World]
 //        asyncApplicationEventByAnnotationDefaultThreadFactory();
+        // @Autowired方式配置 Multicaster时，事件发生异常 java.lang.RuntimeException: 发生异常....
+        // [线程 ：my-event-thread-pool-annotation1] 监听到事件：com.xiaobei.spring.demo.event.MyApplicationEvent[source=Hello, World]
 //        asyncApplicationEventByAnnotationCustomizedThreadFactory();
     }
 
@@ -63,9 +79,15 @@ public class AsyncEventHandlerDemo {
             if (applicationEventMulticaster instanceof SimpleApplicationEventMulticaster) {
                 SimpleApplicationEventMulticaster multicaster = (SimpleApplicationEventMulticaster) applicationEventMulticaster;
                 ExecutorService taskExecutor = Executors.newSingleThreadExecutor();
+                // 为当前的 {@code ApplicationEventMulticaster} 添加线程池
                 multicaster.setTaskExecutor(taskExecutor);
+                // 为当前的 {@code ApplicationEventMulticaster} 添加异常处理
+                multicaster.setErrorHandler(t -> System.err.printf("当前操作发生异常，异常信息为：%s\n", t));
                 shutDownThePool(applicationEventMulticaster, taskExecutor);
             }
+            applicationContext.addApplicationListener((ApplicationListener<MyApplicationEvent>) event -> {
+                throw new RuntimeException("发生异常");
+            });
         });
     }
 
@@ -100,8 +122,13 @@ public class AsyncEventHandlerDemo {
                 SimpleApplicationEventMulticaster multicaster = (SimpleApplicationEventMulticaster) applicationEventMulticaster;
                 ExecutorService taskExecutor = Executors.newSingleThreadExecutor(new CustomizableThreadFactory("my-event-thread-pool"));
                 multicaster.setTaskExecutor(taskExecutor);
+                // 为当前的 {@code ApplicationEventMulticaster} 添加异常处理
+                multicaster.setErrorHandler(t -> System.err.printf("当前操作发生异常，异常信息为：%s\n", t));
                 shutDownThePool(applicationEventMulticaster, taskExecutor);
             }
+            applicationContext.addApplicationListener((ApplicationListener<MyApplicationEvent>) event -> {
+                throw new RuntimeException("发生异常");
+            });
         });
     }
 
@@ -154,6 +181,22 @@ public class AsyncEventHandlerDemo {
 
     static class SyncApplicationEventConfig {
 
+        @Autowired
+        private ApplicationEventMulticaster applicationEventMulticaster;
+
+        @PostConstruct
+        public void initErrorHandler() {
+            if(applicationEventMulticaster instanceof SimpleApplicationEventMulticaster) {
+                SimpleApplicationEventMulticaster multicaster = (SimpleApplicationEventMulticaster) applicationEventMulticaster;
+                multicaster.setErrorHandler(t -> System.err.printf("@Autowired方式配置 Multicaster时，事件发生异常 %s\n", t));
+            }
+        }
+
+        @EventListener
+        public void error(MyApplicationEvent event) {
+            throw new RuntimeException("发生异常....");
+        }
+
         @EventListener
         public void applicationHandler(MyApplicationEvent event) {
             System.out.printf("[线程 ：%s] 监听到事件：%s\n", Thread.currentThread().getName(), event);
@@ -176,6 +219,22 @@ public class AsyncEventHandlerDemo {
 
     @EnableAsync
     static class AsyncApplicationEventConfig {
+
+        @Autowired
+        private ApplicationEventMulticaster applicationEventMulticaster;
+
+        @PostConstruct
+        public void initErrorHandler() {
+            if(applicationEventMulticaster instanceof SimpleApplicationEventMulticaster) {
+                SimpleApplicationEventMulticaster multicaster = (SimpleApplicationEventMulticaster) applicationEventMulticaster;
+                multicaster.setErrorHandler(t -> System.err.printf("@Autowired方式配置 Multicaster时，事件发生异常 %s\n", t));
+            }
+        }
+
+        @EventListener
+        public void error(MyApplicationEvent event) {
+            throw new RuntimeException("发生异常....");
+        }
 
         @EventListener
         @Async
@@ -202,6 +261,21 @@ public class AsyncEventHandlerDemo {
     @EnableAsync
     static class AsyncApplicationEventConfigIncludeCustomizedThreadFactory {
 
+        @Autowired
+        private ApplicationEventMulticaster applicationEventMulticaster;
+
+        @PostConstruct
+        public void initErrorHandler() {
+            if(applicationEventMulticaster instanceof SimpleApplicationEventMulticaster) {
+                SimpleApplicationEventMulticaster multicaster = (SimpleApplicationEventMulticaster) applicationEventMulticaster;
+                multicaster.setErrorHandler(t -> System.err.printf("@Autowired方式配置 Multicaster时，事件发生异常 %s\n", t));
+            }
+        }
+
+        @EventListener
+        public void error(MyApplicationEvent event) {
+            throw new RuntimeException("发生异常....");
+        }
         @EventListener
         @Async
         public void applicationHandler(MyApplicationEvent event) {
