98b86ac63a6e85700deac0a200526fa034dfd535
xiaobei <legend0508@163.com>
2020-09-07 07:39:55 +0800

    spring lookup 完善

--- dependency-lookup/src/main/java/com/xiaobei/spring/demo/dependency/lookup/HierarchicalDependencyLookupDemo.java
+++ dependency-lookup/src/main/java/com/xiaobei/spring/demo/dependency/lookup/HierarchicalDependencyLookupDemo.java
@@ -46,11 +46,22 @@ public class HierarchicalDependencyLookupDemo {
         applicationContext.close();
     }
 
+    /**
+     * 在所有上下文中是否包含指定名称的bean
+     * @param beanFactory
+     * @param beanName
+     */
     private void displayContainsBean(HierarchicalBeanFactory beanFactory, String beanName) {
         System.out.printf("当前BeanFactory[%s]是否包含bean[%s] : %s\n", beanFactory, beanName,
                 containsBean(beanFactory, beanName));
     }
 
+    /**
+     * 递归查找所有上下文中是否存在指定名称的bean
+     * @param beanFactory
+     * @param beanName
+     * @return
+     */
     private boolean containsBean(HierarchicalBeanFactory beanFactory, String beanName) {
         BeanFactory parentBeanFactory = beanFactory.getParentBeanFactory();
         if(parentBeanFactory instanceof HierarchicalBeanFactory) {
@@ -77,16 +88,6 @@ public class HierarchicalDependencyLookupDemo {
         reader.loadBeanDefinitions(location);
         return beanFactory;
     }
-    /**
-     * 通过java代码构造另一个应用上下文
-     * @return
-     */
-    public ConfigurableListableBeanFactory getBeanFactoryWithAnnotation() {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(AncestorsBeanConf.class);
-        applicationContext.refresh();
-        return applicationContext.getBeanFactory();
-    }
 
     /**
      * 先从子应用上下文中查找相应的bean，如果子上下文中没有，再在父上下文中查找
@@ -103,45 +104,40 @@ public class HierarchicalDependencyLookupDemo {
         System.out.println(history);
         Lesson chinese = getBean(beanFactory, "chinese", Lesson.class);
         System.out.println(chinese);
-        Lesson english = getBeanFromAllApplicationContext(beanFactory, "english", Lesson.class);
+        Lesson english = getBean(beanFactory, "english", Lesson.class);
         System.out.println(english);
         applicationContext.close();
     }
 
-
     /**
-     * 从所有的spring应用上下文中查找对应名称和类型的bean
-     * @param beanFactory
-     * @param beanName
-     * @param requiredType
+     * <h2>步骤如下：</h2>
+     * <p> 1. 判断当前应用上下文中是否包含指定名称的bean
+     * <p> 1.2. 如果包含，直接返回
+     * <p> 1.3. 如果不包含，判断父应用上下文是否属于层次性应用上下文
+     * <p> 1.3.1. 如果是，就把父应用上下文转换为层次性应用上下文
+     * <p> 1.3.2. 递归执行步骤 1
+     * <p> 1.4. 如果不包含且父应用上下文也不属于层次性应用上下文，直接返回null
+     * @param beanFactory 当前的层次性应用上下文
+     * @param beanName 指定 bean 的名称
+     * @param requiredType 指定 bean 的类型
      * @param <T>
      * @return
      */
-    <T> T getBeanFromAllApplicationContext(HierarchicalBeanFactory beanFactory, String beanName, Class<T> requiredType) {
-        Assert.notNull(beanFactory, "HierarchicalBeanFactory must not be null");
-        T bean = null;
-        if(beanFactory.containsLocalBean(beanName)) {
-            bean = beanFactory.getBean(beanName, requiredType);
-        }
-        T parentBean = getBean(beanFactory, beanName, requiredType);
-        return parentBean == null ? bean : parentBean;
-    }
-
     <T> T getBean(HierarchicalBeanFactory beanFactory, String beanName, Class<T> requiredType) {
+        // 从当前应用上下文中获取指定的bean
+        if(beanFactory.containsLocalBean(beanName)) {
+            return beanFactory.getBean(beanName, requiredType);
+        }
         if(beanFactory.getParentBeanFactory() instanceof HierarchicalBeanFactory) {
             HierarchicalBeanFactory parentHierarchicalBeanFactory = (HierarchicalBeanFactory) beanFactory.getParentBeanFactory();
-            if(!parentHierarchicalBeanFactory.containsLocalBean(beanName)) {
-                return getBean(parentHierarchicalBeanFactory, beanName, requiredType);
-            } else {
-                return parentHierarchicalBeanFactory.getBean(beanName, requiredType);
-            }
+            return getBean(parentHierarchicalBeanFactory, beanName, requiredType);
         }
         return null;
     }
 
     /**
-     * 使用{@link BeanFactoryUtils#beanOfTypeIncludingAncestors(ListableBeanFactory, java.lang.Class)}
-     * 在所有上下文中查询指定类型的bean。若父类及子类中同时含有，则会返回父类中的bean
+     * 使用 {@link BeanFactoryUtils#beanOfTypeIncludingAncestors(ListableBeanFactory, java.lang.Class)}
+     * 在所有上下文中查询指定类型的bean。若父类及子类中同时含有，则会返回子类中的bean
      *
      * 执行结果：
      * CustomLesson{id=44, name='集合类型依赖查找——自定义课程'}
@@ -155,32 +151,45 @@ public class HierarchicalDependencyLookupDemo {
         HierarchicalBeanFactory parentBeanFactory = getBeanFactoryWithXml();
         beanFactory.setParentBeanFactory(parentBeanFactory);
         applicationContext.refresh();
-        // 查询当前上下文中的bean
+        // 查询当前上下文中的bean，直接查到，不再查询父应用上下文
         CustomLesson customLesson = BeanFactoryUtils.beanOfTypeIncludingAncestors(beanFactory, CustomLesson.class);
         System.out.println(customLesson);
-        // 查询父上下文中的bean
+        // 先查询当前应用上下文，发现没有相应的bean，再查询父上下文中的bean，找到，直接返回
         LessonPrototype lessonPrototype = BeanFactoryUtils.beanOfTypeIncludingAncestors(beanFactory, LessonPrototype.class);
         System.out.println(lessonPrototype);
         applicationContext.close();
     }
     /**
-     * TODO 这个是 “双亲委派” 吗？怎么感觉这个和双亲的逻辑是反的呢？
+     * <p>这个也可以看成是一种“双亲委派”，只是它和classloader的“双亲委派”是相反的。
+     *
      * 根据Bean类型查找实例列表
      * 使用{@link BeanFactoryUtils#beansOfTypeIncludingAncestors(ListableBeanFactory, java.lang.Class)}
      * 查找所有应用上下文中含有的指定类型的bean实例集合
      */
     @Test
-    public void getBeanByTypeIncludingAncestors() {
+    public void getBeansOfTypeIncludingAncestors() {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(ListableBeanConf.class);
         ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
         HierarchicalBeanFactory parentBeanFactory = getBeanFactoryWithXml();
         beanFactory.setParentBeanFactory(parentBeanFactory);
         applicationContext.refresh();
-        // 查询所有应用上下文中的bean
+        // 查询所有应用上下文中的bean，父子应用上下文中同时含有的，父应用上下文中的bean会被覆盖
         Map<String, Lesson> lessonMap = BeanFactoryUtils.beansOfTypeIncludingAncestors(beanFactory, Lesson.class);
         System.out.println(lessonMap);
         applicationContext.close();
+    }
+
+
+    /**
+     * 通过java代码构造另一个应用上下文
+     * @return
+     */
+    public ConfigurableListableBeanFactory getBeanFactoryWithAnnotation() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(AncestorsBeanConf.class);
+        applicationContext.refresh();
+        return applicationContext.getBeanFactory();
     }
 
     /**
@@ -189,7 +198,7 @@ public class HierarchicalDependencyLookupDemo {
      * 来获取所有应用上下文中含有指定注解标记的bean
      */
     @Test
-    public void getBeanNamesForTypeIncludingAncestors() {
+    public void getBeanNamesForAnnotationIncludingAncestors() {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(ListableBeanConf.class);
         ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
@@ -198,7 +207,51 @@ public class HierarchicalDependencyLookupDemo {
         applicationContext.refresh();
         // 查询所有应用上下文中的含有@MyLesson标记的bean
         String[] beanNames = BeanFactoryUtils.beanNamesForAnnotationIncludingAncestors(beanFactory, MyLesson.class);
+        // 自已调整后的逻辑，当父
+        String[] myBeanNames = beanNamesForAnnotationIncludingAncestors(beanFactory, MyLesson.class);
         System.out.println(Arrays.toString(beanNames));
+        System.out.println(Arrays.toString(myBeanNames));
         applicationContext.close();
     }
+
+    /**
+     * 逻辑调整为：如果父、子应用上下文中含有同类型、同名称的bean，但只有父上下文中存在注解标记，那将父应用上下文也查出来
+     * @param lbf
+     * @param annotationType
+     * @return
+     */
+    public static String[] beanNamesForAnnotationIncludingAncestors(
+            ListableBeanFactory lbf, Class<? extends Annotation> annotationType) {
+
+        Assert.notNull(lbf, "ListableBeanFactory must not be null");
+        String[] result = lbf.getBeanNamesForAnnotation(annotationType);
+        if (lbf instanceof HierarchicalBeanFactory) {
+            HierarchicalBeanFactory hbf = (HierarchicalBeanFactory) lbf;
+            if (hbf.getParentBeanFactory() instanceof ListableBeanFactory) {
+                String[] parentResult = beanNamesForAnnotationIncludingAncestors(
+                        (ListableBeanFactory) hbf.getParentBeanFactory(), annotationType);
+                result = mergeNamesIncludeAnnotationWithParent(result, parentResult, hbf, annotationType);
+            }
+        }
+        return result;
+    }
+
+    private static String[] mergeNamesIncludeAnnotationWithParent(String[] result,
+                                                                  String[] parentResult,
+                                                                  HierarchicalBeanFactory hbf,
+                                                                  Class<? extends Annotation> annotationType) {
+        if (parentResult.length == 0) {
+            return result;
+        }
+        List<String> merged = new ArrayList<>(result.length + parentResult.length);
+        merged.addAll(Arrays.asList(result));
+        for (String beanName : parentResult) {
+            if (!merged.contains(beanName) && (!hbf.containsLocalBean(beanName)
+                    || hbf instanceof ListableBeanFactory && ((ListableBeanFactory) hbf).findAnnotationOnBean(beanName, annotationType) == null)) {
+                merged.add(beanName);
+            }
+        }
+        return StringUtils.toStringArray(merged);
+    }
+
 }
