3f19f9bea03935db92edbdf5c0e5c0e298113d89
xiaobei <legend0508@163.com>
2020-12-30 07:45:39 +0800

    reactor?

---
+++ spring-intergration-rest/src/main/java/com/xiaobei/spring/demo/reactive/HttpHandlerConfig.java
@@ -0,0 +1,16 @@
+package com.xiaobei.spring.demo.reactive;
+
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+
+/**
+ * @author <a href="https://github.com/xiaobei-ihmhny">xiaobei-ihmhny</a>
+ * @date 2020/12/30 07:06
+ */
+public class HttpHandlerConfig {
+
+    public void test() {
+//        HttpHandler httpHandler =
+
+    }
+}
